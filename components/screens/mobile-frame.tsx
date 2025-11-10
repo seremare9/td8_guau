c2a7cfbc93734b2931f44f9e45b675c5731d9f78
jahdejosefina <jahdejosefina@hotmail.com>
2025-11-10 12:19:13 -0300

    saque el marco negro y el dynamic island

--- components/screens/mobile-frame.tsx
+++ components/screens/mobile-frame.tsx
@@ -6,10 +6,7 @@ interface MobileFrameProps {
 
 export default function MobileFrame({ children }: MobileFrameProps) {
   return (
-    <div className="max-w-sm mx-auto bg-white min-h-screen relative overflow-hidden border border-black rounded-3xl">
-      {/* Notch */}
-      <div className="absolute top-0 left-1/2 transform -translate-x-1/2 w-32 h-6 bg-black rounded-b-2xl"></div>
-
+    <div className="max-w-sm mx-auto bg-white min-h-screen relative overflow-hidden">
       {children}
     </div>
   );
